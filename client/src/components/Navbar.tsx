aa9aefc0e92a3554ab440395572fec666c417351
Afaq-Hash <afaq23994@gmail.com>
2026-07-25 14:51:47 +0500

    hero-section change

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 import { motion } from "motion/react";
 import { navlinks } from "../data/navlinks";
 import type { INavLink } from "../types";
-import { NavLink } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 export default function Navbar() {
     const [isOpen, setIsOpen] = useState(false);
@@ -16,20 +16,21 @@ export default function Navbar() {
                 viewport={{ once: true }}
                 transition={{ type: "spring", stiffness: 250, damping: 70, mass: 1 }}
             >
-                <a href="https://prebuiltui.com?utm_source=pixels">
-                    <img className="h-8.5 w-auto" src="/assets/logo.svg" alt="logo" width={130} height={34} />
-                </a>
+
+                <img className="h-8.5 w-auto" src="/logo.svg" alt="logo" width={130} height={34} />
+
 
                 <div className="hidden md:flex items-center gap-8 transition duration-500">
-                    {navlinks.map((link: INavLink) => (
-                        <NavLink key={link.name} to={link.href} className="hover:text-pink-500 transition">
-                            {link.name}
-                        </NavLink>
-                    ))}
+
+                    <Link to='/' className="hover:text-pink-500 transition">Home</Link>
+                    <Link to='/Generate' className="hover:text-pink-500 transition">Generate</Link>
+                    <Link to='/My-Generation' className="hover:text-pink-500 transition">My Generations</Link>
+                    <Link to='#' className="hover:text-pink-500 transition">My Contect</Link>
+
                 </div>
 
                 <button className="hidden md:block px-6 py-2.5 bg-pink-600 hover:bg-pink-700 active:scale-95 transition-all rounded-full">
-                    Start free trial
+                    Get Started
                 </button>
                 <button onClick={() => setIsOpen(true)} className="md:hidden">
                     <MenuIcon size={26} className="active:scale-90 transition" />
@@ -37,11 +38,14 @@ export default function Navbar() {
             </motion.nav>
 
             <div className={`fixed inset-0 z-100 bg-black/40 backdrop-blur flex flex-col items-center justify-center text-lg gap-8 md:hidden transition-transform duration-400 ${isOpen ? "translate-x-0" : "-translate-x-full"}`}>
-                {navlinks.map((link: INavLink) => (
-                    <NavLink key={link.name} to={link.href} onClick={() => setIsOpen(false)}>
-                        {link.name}
-                    </NavLink>
-                ))}
+                <Link onClick={() => setIsOpen(false)} to='/' className="hover:text-pink-500 transition">Home</Link>
+                <Link onClick={() => setIsOpen(false)} to='/Generate' className="hover:text-pink-500 transition">Generate</Link>
+                <Link onClick={() => setIsOpen(false)} to='/My-generation' className="hover:text-pink-500 transition">My Generations</Link>
+                <Link onClick={() => setIsOpen(false)} to='#' className="hover:text-pink-500 transition">My Contect</Link>
+                <Link onClick={() => setIsOpen(false)} to='/login' className="hover:text-pink-500 transition">Login</Link>
+
+
+
                 <button onClick={() => setIsOpen(false)} className="active:ring-3 active:ring-white aspect-square size-10 p-1 items-center justify-center bg-pink-600 hover:bg-pink-700 transition text-white rounded-md flex">
                     <XIcon />
                 </button>
