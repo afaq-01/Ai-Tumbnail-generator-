17f21a33fe04e624120c514cabd1620924e22008
Afaq-Hash <afaq23994@gmail.com>
2026-08-03 19:49:11 +0500

    funcationlity add to button

--- client/src/components/previewPanel.tsx
+++ client/src/components/previewPanel.tsx
@@ -1,6 +1,6 @@
 import { div } from "motion/react-client";
 import type { AspectRatio, IThumbnail } from "../assets/assets"
-import { DownloadIcon, Loader2Icon } from "lucide-react";
+import { DownloadIcon, ImageIcon, Loader2Icon } from "lucide-react";
 
 
 const PreviewPanle = ({ thumbnail, isloading, aspectRatio }: { thumbnail: IThumbnail, isloading: boolean; aspectRation: AspectRatio }) => {
@@ -10,6 +10,13 @@ const PreviewPanle = ({ thumbnail, isloading, aspectRatio }: { thumbnail: IThumb
         '9:16': 'aspect-[9/16]',
 
     } as Record<AspectRatio, String>
+
+    const onDownload = () => {
+        if (!thumbnail?.image_url) return;
+        window.open(thumbnail.image_url, '_blank')
+
+    }
+
     return (
         <>
             <div className="relative max-auto w-full  max-w-2xl">
@@ -30,11 +37,25 @@ const PreviewPanle = ({ thumbnail, isloading, aspectRatio }: { thumbnail: IThumb
                         <div className="group relative h-full w-full">
                             <img src={thumbnail?.image_url} alt={thumbnail.title} className="h-full w-full object-cover" />
                             <div className="absalute insert-0 flex items-end justify-center bg-black/10 opacity-0 transition-opacity group-hover:opacity-100">
-                                <button type="button" className="mb-6 flex items-center  gap-2 rounded-md  px-5 py-2.5 text-xs font-meduim transition  bg-white/30 ring-2 ring-white/40 backdrop-blur  hover:scale-105 active:scale-95 ">
+                                <button type="button" onClick={onDownload} className="mb-6 flex items-center  gap-2 rounded-md  px-5 py-2.5 text-xs font-meduim transition  bg-white/30 ring-2 ring-white/40 backdrop-blur  hover:scale-105 active:scale-95 ">
                                     <DownloadIcon className="size-4" />
                                     Download thumbnail
                                 </button>
                             </div>
+                        </div>
+                    )}
+
+                    {/*Empty state */}
+                    {!isloading && !thumbnail?.image_url && (
+                        <div className="absolute inset-0 m-2 flex flex-col items-center justify-center gap-4 rounded-lg border-2 border-dashed border-white/20 bg-black/25">
+                            <div>
+                                <ImageIcon className="size-10 text-white opacity-50" />
+                            </div>
+                            <div className="px-4 text-center">
+                                <p className="font text-zinc-200">Generate your  first thumbnail</p>
+                                <p className="mt-1 text-xs text-zinc-400">fill out the form and click Generate</p>
+                            </div>
+
                         </div>
                     )}
 
